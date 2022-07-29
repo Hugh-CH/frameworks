4d6b137553c9c778d34b00bb92892e8c431b4ceb
HugHud <hugh.hudson@softwire.com>
2022-07-29 15:03:20 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ counter , setCounter ] = useState(0);
+
+  const incrementCounter = ():void => {
+    setCounter(counter+1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          {counter}
+        </h1>
+        <button onClick={incrementCounter}>
+          Add one
+        </button>
       </header>
     </div>
   );
